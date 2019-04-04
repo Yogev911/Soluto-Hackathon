2e8d69a2f19d62c7631ba7a48b62a243510aee78
unknown <extrememax3@gmail.com>
2019-04-05 02:18:20 +0300

    login works with db, added bson to json util

--- backend/api.py
+++ backend/api.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_restful_swagger_2 import Api
 
 from backend import conf
-from backend.resources.products import Products, Product
+from backend.resources.products import Products
 from backend.resources.users import Users
 from backend.resources.login.login import Login
 
@@ -12,7 +12,7 @@ CORS(app)
 api = Api(app, api_version='0.1')
 
 api.add_resource(Products, "/products")
-api.add_resource(Product, "/product/<int:product_id>/like")
+#api.add_resource(Product, "/product/<int:product_id>/like")
 api.add_resource(Users, "/users")
 api.add_resource(Login, "/login")
 
