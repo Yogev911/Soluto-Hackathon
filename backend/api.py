6e7a6b3a8901edec7e88b9c14a10e23958ef8c32
unknown <extrememax3@gmail.com>
2019-04-05 01:18:41 +0300

    added login route without db confirmation

--- backend/api.py
+++ backend/api.py
@@ -3,16 +3,18 @@ from flask import Flask
 from flask_restful_swagger_2 import Api
 
 from backend import conf
-from backend.resources.products import Products, ProductsLikes
+from backend.resources.products import Products, Product
 from backend.resources.users import Users
+from backend.resources.login.login import Login
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app, api_version='0.1')
 
-api.add_resource(ProductsLikes, "/products/<int:product_id>/<int:like>")
 api.add_resource(Products, "/products")
+api.add_resource(Product, "/product/<int:product_id>/like")
 api.add_resource(Users, "/users")
+api.add_resource(Login, "/login")
 
 if __name__ == '__main__':
     app.run(host=conf.HOST, port=conf.PORT)
