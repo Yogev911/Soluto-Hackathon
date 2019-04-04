5e9c79882bd004437d9eb417dd37e3112df36995
Yogev Heskia <Yogev911@gmail.com>
2019-04-05 00:56:10 +0300

    init api

--- backend/api.py
+++ backend/api.py
@@ -3,15 +3,15 @@ from flask import Flask
 from flask_restful_swagger_2 import Api
 
 from backend import conf
-from backend.resources.products import Products, Product
+from backend.resources.products import Products, ProductsLikes
 from backend.resources.users import Users
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app, api_version='0.1')
 
+api.add_resource(ProductsLikes, "/products/<int:product_id>/<int:like>")
 api.add_resource(Products, "/products")
-api.add_resource(Product, "/product/<int:product_id>/like")
 api.add_resource(Users, "/users")
 
 if __name__ == '__main__':
