5b94670ba3d43c70e76ad82215da7d1145dce958
Yogev <Yogev911@gmail.com>
2019-04-04 21:10:28 +0300

    init api

--- api/api.py
+++ api/api.py
@@ -1,27 +1,17 @@
-import os
-import json
 from flask_cors import CORS
-from flask import Flask, request, jsonify
-import traceback
+from flask import Flask
 from flask_restful_swagger_2 import Api
-import api_handler
-import conf
-import utils
+
+
+from api import conf
+from api.resources.products import Products
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app, api_version='0.1')
 
 api.add_resource(Products,"/products")
-@app.route('/init', methods=['GET', 'POST'])
-def init():
-    try:
-        api_handler.db.init_db()
-        return utils.create_res_obj({'status': 'init success'})
-    except Exception as e:
-        return utils.create_res_obj(
-            {'traceback': traceback.format_exc(), 'msg': "{}".format(e.args)},
-            success=False)
+
 
 
 def run():
