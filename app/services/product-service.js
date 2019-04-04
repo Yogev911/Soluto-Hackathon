e5cf4f9b30c12fdea2f6a5238ca96c6b02399fc4
oriamd <oria@rapyd.net>
2019-04-05 01:52:46 +0300

    Added dialog

--- app/services/product-service.js
+++ app/services/product-service.js
@@ -3,6 +3,9 @@ import img2 from '../assets/image2.jpeg';
 import img3 from '../assets/image3.jpeg';
 import img4 from '../assets/image4.jpeg';
 import img5 from '../assets/image5.jpeg';
+import {users, UserService} from './user-service';
+import axios from 'axios';
+
 export class Product {
     name;
     photo;
@@ -16,9 +19,17 @@ export class Product {
 
 
 export class ProductService {
-    baseUrl = '/products';
+    baseUrl = 'http://192.168.43.228:8080'
+    objectUrl = '/products';
 
     static products = [];
+
+    getUserId(){
+        if(users.loggedInUser){
+            retu
+        }
+        return 
+    }
 
     async getProducts(){
         const cards = [
@@ -28,15 +39,31 @@ export class ProductService {
             { id: '4', image: img4, isActive: false },
             { id: '5', image: img5, isActive: false },
         ];
+        //return axios.get(url,{headers:{user_id:user_id}})
         return cards;
         //return ProductService.products;
     }
 
+    async updateProduct(productId, isLike){
+        if(!productId){
+            throw "ERROR - invalid product id";
+        }
+
+        let url = `${baseUrl}${objectUrl}/${productId}/${isLike?1:2}`;
+        let user_id = this.getUserId();
+        
+        return axios.get(url,{headers:{user_id:user_id}})
+    }
+
     async likeProduct(productId){
-        return true
+        console.log('ProductService.likeProduct called')
+        return true;
+        return this.updateProduct(productId, true)
     }
 
     async dislikeProduct(productId){
-        return true
+        console.log('ProductService.dislikeProduct called')
+        return true;
+        return this.updateProduct(productId, false)
     }
 }
