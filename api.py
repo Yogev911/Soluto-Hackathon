ae1628e7d81fe54f38c7bd39ad44a343e8aabf25
Yogev Heskia <Yogev911@gmail.com>
2019-04-05 03:00:17 +0300

    init api

--- api.py
+++ api.py
@@ -3,11 +3,7 @@ from flask import Flask
 from flask_restful_swagger_2 import Api
 
 from backend import conf
-<<<<<<< HEAD:api.py
-=======
-from backend.resources.products import Products
-from backend.resources.users import Users
->>>>>>> login_backend:backend/api.py
+
 from backend.resources.login.login import Login
 from backend.resources.products.products import Products, ProductsLikes
 from backend.resources.users import Users
@@ -18,7 +14,6 @@ api = Api(app, api_version='0.1')
 
 api.add_resource(ProductsLikes, "/products/<int:product_id>/<int:like>")
 api.add_resource(Products, "/products")
-#api.add_resource(Product, "/product/<int:product_id>/like")
 api.add_resource(Users, "/users")
 api.add_resource(Login, "/login")
 
